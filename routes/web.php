c98601cd96b380eeb6a1d639930896e9f1a1be53
idn-fahmifn <fahmifn.idn@gmail.com>
2025-11-29 13:17:29 +0700

    1.3

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HalamanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome'); 
+    return view('welcome');
 });
 
 // Basic Routing in Laravel
@@ -13,25 +13,48 @@ Route::get('/', function () {
 // resource = operasi CRUD
 
 Route::get('profile', [HalamanController::class, 'profile'])
-->name('halaman.profile'); //hanya nama
+    ->name('halaman.profile'); //hanya nama
 
 
 Route::get('home', [HalamanController::class, 'home'])
-->name('halaman.home'); //hanya nama
+    ->name('halaman.home'); //hanya nama
 
 
 // Route dengan Parameter
 //  - Parameter wajib
-Route::get('mobil/{param}', function($param){
+Route::get('mobil/{param}', function ($param) {
     $merk = $param;
-    return 'Ini adalah detail mobil '.$merk;
+    return 'Ini adalah detail mobil ' . $merk;
 })->name('mobil');
 
 // - parameter Optional
-Route::get('motor/{param?}', function($param = 'honda'){
+Route::get('motor/{param?}', function ($param = 'honda') {
     $merk = $param;
-    return 'Ini adalah detail motor '.$merk;
+    return 'Ini adalah detail motor ' . $merk;
 })->name('motor');
+
+// Group untun Routing
+// 1. kelompokan berdasarkan prefix
+// 2. kelompokan berdasarkan middleware
+Route::prefix('training')->group(function () {
+
+    Route::get('mtcna', function () {
+        return "Ini adalah kelas mtcna";
+    });
+    Route::get('ccna', function () {
+        return "Ini adalah kelas ccna";
+    });
+    Route::get('mtcine', function () {
+        return "Ini adalah kelas mtcine";
+    });
+    Route::prefix('programming')->group(function () {
+        Route::get('laravel', function () {
+            return "Ini adalah kelas laravel";
+        });
+    });
+});
+
+
 
 
 
