8898c82e46be89641102ce3e7490ccb52939a0f1
idn-fahmifn <fahmifn.idn@gmail.com>
2025-11-29 16:27:42 +0700

    1.9

--- app/Http/Controllers/UmurController.php
+++ app/Http/Controllers/UmurController.php
@@ -21,12 +21,22 @@ class UmurController extends Controller
         $request->validate([
             'nama' => ['required', 'string', 'min:3', 'max:30'],
             'umur' => ['required', 'integer', 'min:1', 'max:99'] //20
+        ],[
+            'nama.required' => "Nama wajib diisi",
+            'nama.string' => "Nama isisnya adalah karakter",
+            'nama.min' => "Masukan minimal 3 karakter",
+            'nama.max' => "Masukan maksimal 30 karakter",
+
+            'umur.required' => "Umur wajib diisi",
+            'umur.integer' => "Umur isinya adalah angka",
+            'umur.min' => "Masukan minimal Umur 1",
+            'umur.max' => "Masukan maksimal Umur 99",
         ]);
 
         // session agar data umur bisa diperiksa oleh middleware.
 
         $request->session()->put('age', $request->umur);
-        
+
         return redirect()->route('umur.success');
 
     }
