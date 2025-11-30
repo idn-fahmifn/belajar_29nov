1ac25da047759dd49a08052e7a3a31f5012b3a0f
idn-fahmifn <fahmifn.idn@gmail.com>
2025-11-30 11:02:29 +0700

    2.2

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -6,6 +6,32 @@
             <div class="card-header">
                 <div class="card-title">Data Semua Barang</div>
             </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <th>Nama Barang</th>
+                            <th>Merk</th>
+                            <th>Harga</th>
+                            <th>Action</th>
+                        </thead>
+                        <tbody>
+                            {{-- foreach / forelse. --}}
+                            @forelse($data as $item)
+                            <tr>
+                                <td>{{ $item->nama_barang }}</td>
+                                <td>{{ $item->merk }}</td>
+                                <td>IDR. {{ number_format($item->harga) }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-info btn-sm">Detail</a>
+                                </td>
+                            </tr>
+                            @empty
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
